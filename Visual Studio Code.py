fe1e605c9e5ca38831c5d87c240db0969b3a364a
AbdulHannanAhmed <117820151+AbdulHannanAhmed@users.noreply.github.com>
2022-11-09 17:04:52 +0500

    Create Visual Studio Code.py
    This is a simple code written in Python on how to draw a logo of Visual Studio Code.

---
+++ Visual Studio Code.py
@@ -0,0 +1,18 @@
+import turtle
+from turtle import *
+
+turtle.speed('slowest')
+
+color('blue')
+bgcolor('black')
+
+turtle.pensize(50)
+
+left(30)
+forward(250)
+right(120)
+forward(150)
+right(120)
+forward(250)
+
+done()
